8ec5a907152627e857a51aa799d2a007523fcbe3
Diogodhs <dhs@cin.ufpe.br>
2019-05-27 23:24:22 -0300

    Add files via upload

--- rfid/rfid1.ino
+++ rfid/rfid1.ino
@@ -20,9 +20,9 @@ MFRC522 mfrc522[numReaders];
 const String correctIDs[] = {""};
 //As tags IDs detectada atualmente
 String currentIDs[numReaders];
-//A saída para a resolução do puzzle
-const byte lockPin = A0;
-
+//Pacote com todas as IDs em um único String
+String pacote;
+String IDs[numReaders];
 
 void setup() 
 {
@@ -32,12 +32,13 @@ void setup()
   Serial.println(F("Comunicação Serial inicializado"));
   #endif
 
-  //Setar a trava do puzzle
-  pinMode(lockPin, OUTPUT);
-  digitalWrite(lockPin, HIGH);
-
   //Inicializa o SPI bus
   SPI.begin();
+
+  //Inicializa as IDs
+  for(uint8_t i=0; i<numReaders; i++){
+    IDs[i] = "xxxxxxxx";
+  }
 
   for(uint8_t i=0; i<numReaders; i++){
 
@@ -66,9 +67,7 @@ String dump_byte_array(byte *buffer, byte bufferSize) {
 
   String conteudo = "";
   for (byte i = 0; i < bufferSize; i++) {
-    Serial.print(buffer[i] < 0x10 ? " 0" : " ");
-    Serial.print(buffer[i], HEX);
-    conteudo.concat(String(buffer[i] < 0x10 ? " 0" : " "));
+    conteudo.concat(String(buffer[i] < 0x10 ? "0" : ""));
     conteudo.concat(String(buffer[i], HEX));
   }
   
@@ -77,9 +76,6 @@ String dump_byte_array(byte *buffer, byte bufferSize) {
  
 void loop() 
 {
-
-  //Assumindo que o puzzle está resolvido
-  boolean puzzleSolved = true;
 
   //Assumindo que as tags não mudaram desde a ultima leitura
   boolean changedValue = false;
@@ -92,12 +88,43 @@ void loop()
 
     //String com a ID de cada sensor
     String readRFID = "";
+    //Montagem do print no Serial
+    String printID = "ID ";
+    printID.concat(i);
+    printID.concat(": ");
 
-    //Se o sensor detectar uma tag e libera a leitura
+    //Se o sensor detectar uma tag e libera a leitura e imprime no Serial os IDs detectados
     if(mfrc522[i].PICC_IsNewCardPresent() && mfrc522[i].PICC_ReadCardSerial()){
       //Extrai o ID da tag
       readRFID = dump_byte_array(mfrc522[i].uid.uidByte, mfrc522[i].uid.size);
+      printID.concat(readRFID);
+      Serial.println(printID);
     }
+    else{
+      Serial.println(printID);
+    }
+
+    //Empacotamento de IDs
+    IDs[i] = readRFID;
+    pacote = "";
+    for(uint8_t j=0; j<numReaders; j++){
+      if(IDs[j] != ""){
+        if(j == 0 || j == numReaders){
+          pacote.concat(IDs[0]);
+        }else{
+          pacote.concat("%");
+          pacote.concat(IDs[j]);
+        }
+      }else{
+        if(j == 0 || j == numReaders){
+          pacote.concat("xxxxxxxx");
+        }else{
+          pacote.concat("%");
+          pacote.concat("xxxxxxxx");
+        }
+      }
+    }
+    Serial.println(pacote);
 
     //Se a leitura atual é o mesmo do último
     if(readRFID != currentIDs[i]){
@@ -105,12 +132,6 @@ void loop()
       changedValue = true;
       //Atualiza os valores armazenados dos IDs
       currentIDs[i] = readRFID;
-
-      //Se falhar na combinação de IDs
-      if(currentIDs[i] != correctIDs[i]){
-        //O puzzle está errado
-        puzzleSolved = false;
-      }
 
       //Halt PICC
       mfrc522[i].PICC_HaltA();
@@ -128,7 +149,6 @@ void loop()
       Serial.print(F(". Flag detectada: "));
       Serial.print(currentIDs[i]);
     }
-    Serial.println(F("---"));
     delay(1000);
   }
   
